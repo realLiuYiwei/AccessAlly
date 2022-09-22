ddb197b1ae41015ee6335a20879ee38950e5ae76
Liu Yiwei <69023807+realLiuYiwei@users.noreply.github.com>
2022-09-22 17:01:40 -0400

    Update main.py

--- q2/main.py
+++ q2/main.py
@@ -2,7 +2,7 @@ blood = [int(i) for i in input().split()]
 patients = [int(i) for i in input().split()]
 
 total = 0
-for i in range(0, 7):
+for i in range(0, 8):
     mini = min(blood[i], patients[i])
     blood[i] = blood[i] - mini
     patients[i] = patients[i] - mini
@@ -15,7 +15,7 @@ for i in range(0, 7):
                 5: [4, 0, 1],
                 7: [0,1,2,3,4,5,6]}
 
-for i in range(1, 7):
+for i in range(1, 8):
     if patients[i] > 0:
         for bl in need_dic[i]:
             mini = min(blood[bl], patients[i])
